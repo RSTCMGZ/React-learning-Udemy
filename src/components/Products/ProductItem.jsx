fce3315e2cce17d9ad97f6e8f63ba66a363a66a3
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-10-12 01:23:28 +0300

    learnin react event

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -3,8 +3,15 @@ import ProductInfo from "./ProductInfo";
 const ProductItem = ({ product }) => {
   // const { product } = props; //props bu şekildede yazılabilinir.
   // const {imageUrl, productName, productPrice,} = product // böylede yazılır.
+  const clickHandler = () => console.log("Sepete Eklendi!");
 
   // const date = new Date(2023, 2, 20);
+  //!Saf JavaScript
+  // const myButton = document.getElementById("myButton");
+  // myButton.addEventListener("click", () => {
+  //   console.log("Sepete eklendi");
+  // });
+
   return (
     <div className="product-item">
       <div className="product-img">
@@ -16,7 +23,12 @@ const ProductItem = ({ product }) => {
       </div>
       <ProductInfo>
         <h2>{product.productName}</h2>
-        <span>{product.productPrice} TL</span>
+        <span>{product.productPrice} TL</span> <br />
+        {/* <button id="myButton" onClick={() => console.log("Sepete Eklendi!")}>
+          Sepete Ekle
+        </button> */}
+        {/* <button onCanPlay={clickHandler}>Sepete Ekle</button> */}
+        <button onCanPlay={clickHandler}>Güncelle</button>
       </ProductInfo>
     </div>
   );
