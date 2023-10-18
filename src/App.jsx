bba2904139bc4ea5673e0855c639257d236db436
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-10-19 01:06:17 +0300

    add-new-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import "./App.css";
-import Products from "./components/Products/Products.jsx";
+import CustomerForm from "./components/CustomerForm/CustomerForm";
+import CustomerList from "./components/CustomerList/CustomerList";
 
-const App = () => {
+function App() {
+  const [customers, setCustomers] = useState([]);
+
+  const addNewCustomer = (newCustomer) => {
+    // setCustomers([...customers, newCustomer]);
+    setCustomers((prevState) => [newCustomer, ...prevState]);
+  };
+
   return (
     <div className="App">
-      <Products />
+      <h1>Customer Manage System</h1>
+      <CustomerForm addNewCustomer={addNewCustomer} />
+      {customers.length === 0 && "There are no customers..."}
+      <CustomerList customers={customers} setCustomers={setCustomers} />
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./CustomerForm.css";
+
+const CustomerForm = ({ addNewCustomer }) => {
+  const [customerName, setCustomerName] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (customerName.trim().length === 0) {
+      setIsValid(true);
+      return;
+    }
+    const newCustomer = {
+      id: Math.random(),
+      customerName,
+    };
+    addNewCustomer(newCustomer);
+    setCustomerName("");
+  };
+  const nameInputChangeHandler = (e) => {
+    const nameValaue = e.target.value;
+    if (nameValaue.trim().length > 0) {
+      setIsValid(false);
+    }
+    setCustomerName(nameValaue);
+  };
+  return (
+    <form className="customer-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="customer-input"
+        placeholder="Add a new customer"
+        onChange={nameInputChangeHandler}
+        value={customerName}
+        style={{
+          backgroundColor: isValid ? "red" : "",
+        }}
+      />
+      <button>
+        <i className="bi bi-plus-lg"></i>
+      </button>
+    </form>
+  );
+};
+
+export default CustomerForm;
