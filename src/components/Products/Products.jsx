a768f5788a772b425c60beb236302c84a3cb2fd9
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-10-18 21:05:22 +0300

    delete-productData

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,5 @@
 import "../../components/Products/Products.css";
 import ProductItem from "./ProductItem";
-import { productData } from "../../productData";
 import NewProduct from "../NewProduct/NewProduct";
 import { useState } from "react";
 // import ToggleBackground from "./ToggleBackground";
@@ -11,13 +10,20 @@ const Products = () => {
   //   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzNvla5p3z0IUK4ED-I_Y3DyCP5h-MJkLxew&usqp=CAU";
   // const productName = "Bal";
   // const productPrice = 10;
-  const [products, SetProducts] = useState(productData);
+  const [products, SetProducts] = useState([]);
 
   return (
     <div className="product-wrapper">
       <NewProduct products={products} SetProducts={SetProducts} />
       <h1>Products</h1>
       <div className="products ">
+        {products.length === 0 ? (
+          <p>Hiç Ürün Yok.</p>
+        ) : (
+          products.map((product) => (
+            <ProductItem product={product} key={product.id} />
+          ))
+        )}
         {/* //!ikinci yol */}
         {/* {productData.map((product) => {
           return <ProductItem key={product.productName} product={product} />;
@@ -26,9 +32,6 @@ const Products = () => {
           <ProductItem key={product.productName} product={product} />
         ))} */}
         {/* //!ikinci yol */}
-        {products.map((product) => (
-          <ProductItem product={product} key={product.id} />
-        ))}
 
         {/* <ProductItem imageUrl={imageUrl} productName="Süt" productPrice="45" /> */}
         {/* <ToggleBackground /> */}
